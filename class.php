d1dca78bf49ef3f58f89ef598514bc3781dabf85
Timo Tijhof <krinklemail@gmail.com>
2012-06-21 00:32:32 +0200

    remove KrToolBaseClass (now in basetool)

--- class.php
+++ class.php
@@ -8,26 +8,6 @@
  * @license CC-BY-SA 3.0 Unported: creativecommons.org/licenses/by/3.0/
  */
 
-class KrToolBaseClass {
-
-	protected $settings = array();
-
-	protected $settingsKeys = array();
-
-	public function setSettings( $settings ) {
-		foreach ( $this->settingsKeys as $key ) {
-			if ( !array_key_exists( $key, $settings ) ) {
-				throw new InvalidArgumentException( "Settings must have key $key." );
-			}
-		}
-		$this->settings = $settings;
-	}
-
-	public function getSetting( $key ) {
-		return isset( $this->settings[$key] ) ? $this->settings[$key] : null;
-	}
-}
-
 class KrMwSnapshots extends KrToolBaseClass {
 
 	protected $settingsKeys = array(
