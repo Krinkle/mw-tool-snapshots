a658ca4336735523b94240e93d7300ca0b098943
Timo Tijhof <krinklemail@gmail.com>
2014-01-17 18:33:34 +0100

    Clean up for tools.wmflabs.org

--- common.php
+++ common.php
@@ -1,7 +1,7 @@
 <?php
 
 // BaseTool & Localization
-require_once( __DIR__ . '/../ts-krinkle-basetool/InitTool.php' );
+require_once( __DIR__ . '/lib/basetool/InitTool.php' );
 require_once( KR_TSINT_START_INC );
 
 // Class for this tool
@@ -11,15 +11,14 @@ $kgTool = new KrMwSnapshots();
 $I18N = new TsIntuition( 'Mwsnapshots' );
 
 $toolConfig = array(
-	'displayTitle'     => 'mwSnapshots',
-	'remoteBasePath'   => $kgConf->getRemoteBase() . '/mwSnapshots/',
-	'revisionId'       => '0.1.0',
-	'revisionDate'     => '2012-05-14',
-	'I18N'             => $I18N,
-	'styles'           => array(
+	'displayTitle' => 'mwSnapshots',
+	'krinklePrefix' => false,
+	'remoteBasePath' => dirname( $kgConf->getRemoteBase() ). '/',
+	'revisionId' => '0.1.2',
+	'styles' => array(
 		'main.css',
 	),
-	'scripts'          => array(
+	'scripts' => array(
 		'main.js',
 	),
 );
--- sample-local.php
+++ sample-local.php
@@ -9,10 +9,10 @@ $kgTool->setSettings(array(
 	// swapping HEAD and active branch back and forth between very
 	// old versions of MediaWiki, new versions and even potentially
 	// unstable deveopment branches.
-	'mediawikiCoreRepoDir' => '/mnt/user-store/krinkle/mwSnapshots/remotes/mediawiki-core',
+	'mediawikiCoreRepoDir' => __DIR__ . '/remotes/mediawiki-core',
 
 	// This (among other temporary files) where the tarballs will be stored
-	'cacheDir' => '/mnt/user-store/krinkle/mwSnapshots/cache',
+	'cacheDir' => __DIR__ . '/cache',
 
 	// Log files will be written here. Only the log of the last run is kept.
 	'logsDir' => __DIR__ . '/logs',
